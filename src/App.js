f8ba6f0266aa403230278ad848073add9ae1e77a
Maximilian <scientificresearch309@gmail.com>
2023-12-18 00:18:00 +0100

    and now Users in user page is working but with Router, Routes, Route and element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Users } from "./user/pages/Users";
 
 const App = () => {
   // return <h1>Let's start!</h1>;
-  return <Router>
-    
-  </Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
