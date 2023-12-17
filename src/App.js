74312940ebf408e1b999edc93fe0b5e0819481ce
Maximilian <scientificresearch309@gmail.com>
2023-12-18 00:49:59 +0100

    NewPlace page is working too!

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 
 import { Users } from "./user/pages/Users";
+import { NewPlace } from "./places/pages/NewPlace";
 
 const App = () => {
   // return <h1>Let's start!</h1>;
   return (
     <Router>
       <Routes>
+        <Route path="/places/new" element={<NewPlace />}></Route>
         <Route path="/" element={<Users />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
