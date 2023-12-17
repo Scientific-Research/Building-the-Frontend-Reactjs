5e10d6bce4b387c3a92d6d7db1c0ced90415adff
Maximilian <scientificresearch309@gmail.com>
2023-12-18 00:31:20 +0100

    When we enter anything but this "/", it will navigate to the "/" root!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 import { Users } from "./user/pages/Users";
 
@@ -9,6 +14,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Users />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
