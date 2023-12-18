1e1b61ff405a5bf4c4f5f8b19ed22ce44b4f1b4d
Maximilian <scientificresearch309@gmail.com>
2023-12-18 17:58:58 +0100

    Adding a Main Header

--- src/App.js
+++ src/App.js
@@ -8,16 +8,20 @@ import {
 
 import { Users } from "./user/pages/Users";
 import { NewPlace } from "./places/pages/NewPlace";
+import { MainNavigation } from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   // return <h1>Let's start!</h1>;
   return (
     <Router>
-      <Routes>
-        <Route path="/places/new" element={<NewPlace />}></Route>
-        <Route path="/" element={<Users />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/places/new" element={<NewPlace />}></Route>
+          <Route path="/" element={<Users />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
