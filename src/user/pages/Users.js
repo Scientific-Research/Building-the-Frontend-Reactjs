f993f467c68f86aaa5adf9b5f71f7650bc97ddb6
Maximilian <scientificresearch309@gmail.com>
2023-12-18 14:37:47 +0100

    Users => UsersList => UserItem

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,5 +1,16 @@
-import React from "react";
+// import React from "react";
+
+import { UsersList } from "../components/UsersList";
 
 export const Users = () => {
-  return <h2>Users works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image: "https://images.pexels.com/photos/825904/pexels-photo-825904.jpeg",
+      places: 3,
+    },
+  ];
+  // return <h2>Users works!</h2>;
+  return <UsersList items={USERS} />;
 };
