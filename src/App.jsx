45ddf261adc766a60eb31bcf7b7b978d17a00a4f
Maximilian <scientificresearch309@gmail.com>
2023-12-21 16:41:06 +0100

    Adding Auth Context for App-wide State Management

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -12,23 +12,36 @@ import { MainNavigation } from "./shared/components/Navigation/MainNavigation";
 import { UserPlaces } from "./places/pages/UserPlaces";
 import { UpdatePlace } from "./places/pages/UpdatePlace";
 import { Auth } from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
-  // return <h1>Let's start!</h1>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<Users />} />
-          <Route path="/:userId/places" element={<UserPlaces />} />
-          <Route path="/places/new" element={<NewPlace />} />
-          <Route path="/places/:placeId" element={<UpdatePlace />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />} />
+            <Route path="/:userId/places" element={<UserPlaces />} />
+            <Route path="/places/new" element={<NewPlace />} />
+            <Route path="/places/:placeId" element={<UpdatePlace />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
