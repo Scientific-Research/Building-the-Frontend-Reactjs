a886a36503f8b29d9647a8b90adb1fedcb304e91
Maximilian <scientificresearch309@gmail.com>
2023-12-21 22:54:18 +0100

    Creating Logout Button using More Auth Context Usage!

--- src/shared/components/Navigation/NavLinks.jsx
+++ src/shared/components/Navigation/NavLinks.jsx
@@ -12,18 +12,24 @@ export const NavLinks = (props) => {
         <NavLink to="/">ALL USERS</NavLink>
       </li>
       {auth.isLoggedIn && (
-        <li>
-          <NavLink to="/u1/places">MY PLACES</NavLink>
-        </li>
-      )}
-      {auth.isLoggedIn && (
-        <li>
-          <NavLink to="/places/new">ADD PLACE</NavLink>
-        </li>
+        <>
+          <li>
+            <NavLink to="/u1/places">MY PLACES</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/places/new">ADD PLACE</NavLink>
+          </li>
+        </>
       )}
       {!auth.isLoggedIn && (
         <li>
           <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
         </li>
       )}
     </ul>
