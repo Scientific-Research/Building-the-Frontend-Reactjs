2b05e45dbd89e0f283c8ca2d730d8200fd46f6bb
Maximilian <scientificresearch309@gmail.com>
2023-12-31 21:37:16 +0100

    Getting Started With Error Handlin- Showing Spinner!

--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -5,6 +5,9 @@ import "./Auth.css";
 import Card from "../../shared/components/UIElements/Card";
 import { Input } from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_EMAIL,
@@ -17,6 +20,9 @@ export const Auth = () => {
   const auth = useContext(AuthContext);
   // Using our hook here: useForm()
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
@@ -61,6 +67,7 @@ export const Auth = () => {
     if (isLoginMode) {
     } else {
       try {
+        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -74,14 +81,20 @@ export const Auth = () => {
         });
         const responseData = await response.json();
         console.log(responseData);
+        auth.login();
+        setIsLoading(false);
       } catch (err) {
         console.log(err);
+        setIsLoading(false);
+        setError(
+          err.message || "Something went wrong, please try again later!"
+        );
       }
     }
-    auth.login();
   };
   return (
     <Card className="authentication">
+      {isLoading && <LoadingSpinner asOverlay={true} />}
       {/* <h2>Login Required!</h2> */}
       {/* <h2>{isLoginMode ? "Login" : "Signup"} Required!</h2> */}
       <h2>
