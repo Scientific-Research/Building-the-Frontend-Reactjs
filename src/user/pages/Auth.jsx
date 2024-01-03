1e8f08bc63564c5f45ce7db809b385a1f5d7b55f
Maximilian <scientificresearch309@gmail.com>
2024-01-03 10:55:49 +0100

    Building an ImageUpload Component

--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -7,6 +7,7 @@ import { Input } from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { ImageUpload } from "../../shared/components/FormElements/ImageUpload";
 
 import {
   VALIDATOR_MINLENGTH,
@@ -168,6 +169,7 @@ export const Auth = () => {
               onInput={inputHandler}
             />
           )}
+          {!isLoginMode && <ImageUpload center id="image" />}
           <Input
             element="input"
             id="email"
